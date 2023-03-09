dca8aaf74ae8e6b323cf356c6193faf211246a64
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-09 13:49:02 +0200

    HW #37 Solution Files Copying

---
+++ src/telran/io/Copy.java
@@ -0,0 +1,44 @@
+package telran.io;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+abstract public class Copy {
+	protected String srcFilePath;
+	protected String destFilePath;
+	protected boolean overwrite;
+	
+	public Copy(String srcFilePath, String destFilePath, boolean overwrite) {
+		this.srcFilePath = srcFilePath;
+		this.destFilePath = destFilePath;
+		this.overwrite = overwrite;
+	}
+	
+	abstract public long copy();
+	
+	abstract DisplayResult getDisplayResult(long copyTime, long fileSize);
+	
+	public void copyRun() {
+		try {
+			canOverwrite();
+			Instant timestamp = Instant.now();
+			long size = copy();
+			ChronoUnit unit = ChronoUnit.MILLIS;
+			DisplayResult displayResult = getDisplayResult(unit.between(timestamp, Instant.now()),
+					size);
+			System.out.println(displayResult);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	private void canOverwrite() throws Exception  {
+		if (!overwrite && Files.exists(Path.of(destFilePath))) {
+			throw new Exception(destFilePath + " cannot be overwritten");
+		}
+	}
+	
+	
+	
+}
